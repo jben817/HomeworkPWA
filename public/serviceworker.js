3c03bdc180e7a7260d58b6b9ff16cbeb5c7958ff
j.bennett <61255945+jben817@users.noreply.github.com>
2020-07-26 18:42:49 -0400

    typo corrected

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,14 +1,12 @@
 self.addEventListener("install", e =>{
     e.waitUntil(
         caches.open("static").then(cache => {
-            console.log (cache);
            return cache.addAll(["./", "./index.html","./index.js","./styles.css"]) 
         })
     );
 });
 
 self.addEventListener("fetch", e => {
-    console.log(e.request.url);
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch (e.request);
